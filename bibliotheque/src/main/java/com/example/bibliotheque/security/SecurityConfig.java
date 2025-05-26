f4d81039d600fca8ca866a9c796a169d604892b8
totolamatrac <thomasangama77460@gmail.com>
2025-05-26 10:31:30 +0200

    feat: add security and config

---
+++ bibliotheque/src/main/java/com/example/bibliotheque/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.bibliotheque.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/public").permitAll()
+                    .anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http
+            .getSharedObject(AuthenticationManagerBuilder.class)
+            .userDetailsService(userDetailService)
+            .passwordEncoder(passwordEncoder())
+            .and()
+            .build();
+    }
+}
